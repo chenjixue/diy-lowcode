d3fddcecb5b69573b5698b0ccbab9fc6d2cc98c7
chenjixue <736538448@qq.com>
2024-04-15 23:24:30 +0800

    md document modification

--- src/core/area.ts
+++ src/core/area.ts
@@ -1,3 +1,23 @@
-export class Area {
+import { Sketeton } from "./editor-skeleton";
+import { WidgetContainer } from "./widget-container";
+
+export interface IArea<C,T>{
+    add(config:C):T;
+}
+export interface IWidget {
+    name:string
+}
+export interface IPublicTypeWidgetBaseConfig{
     
+}
+export class Area<C extends IPublicTypeWidgetBaseConfig = any,T extends IWidget = IWidget > implements IArea<C,T>{
+    readonly container:WidgetContainer<T>
+    constructor(readonly skeleton:Sketeton,readonly name:string, handle:(item:T)=>T){
+        this.container = Sketeton.createContainer(name,handle)
+    }
+    add(config:C):T{
+        // return this.container.add(confis)
+        return 
+    }
+  
 }
